c9a99f58f8a3a30a1bb5e7eab6f0cd3dd86519f3
arefweb <arefmovahedz@gmail.com>
2022-09-11 11:21:48 +0430

    Readme

--- src/index.tsx
+++ src/index.tsx
@@ -2,27 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css"
-// import { createGlobalStyle } from 'styled-components';
-
-// interface IGlobal{
-//   whiteColor?: {};
-// }
-// const GlobalStyle = createGlobalStyle<IGlobal>`
-//   *{
-//     @import url('./assets/fonts/Roboto-Regular.ttf')
-//     font-family: roboto;
-//   }
-//   body {
-//     background-color: ${props => (props.whiteColor ? 'white' : '#171b26')};
-//     font-family: roboto;
-//   }
-// `
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <App />
-    {/* <GlobalStyle /> */}
   </React.StrictMode>
 );
 
